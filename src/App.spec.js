846dfab578b8a0480dd583fe525cfac2ca6654b4
Eddy Struillou <eddy.s25@hotmail.fr>
2018-05-25 15:47:19 +0200

    Ajout  de sinonJS et snapshot

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,8 +1,9 @@
 import { expect } from 'chai'
 import { shallow } from 'enzyme' 
 import React from 'react'
+import sinon from 'sinon'
 
-import App from './App'
+import App, { SYMBOLS } from './App'
 import GuessCount from './GuessCount';
 
 describe('<App />', () => {
@@ -12,10 +13,20 @@ describe('<App />', () => {
     expect(wrapper).to.contain(<GuessCount guesses={0} />)
   })
 
-  it('has 36 cards', () => {
+  it('had 36 cards', () => {
     const wrapper = shallow(<App />)
   
-    expect(wrapper.find('Card')).to.have.length(36)
+    expect(wrapper.find('Card')).to.have.been.length(36)
   })
 
+  it('should match ots reference snapshot', () => {
+    const mock = sinon.stub(App.prototype, 'generateCards').returns([...SYMBOLS.repeat(2)])
+    try {
+      const wrapper = shallow(<App />)
+  
+      expect(wrapper).to.matchSnapshot()
+    } finally {
+      mock.restore()
+    }
+  })
 })
